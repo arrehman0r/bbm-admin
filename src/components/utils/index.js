e506064f113e5cee77914739b817f2697597993f
umair0033 <you@example.com>
2024-07-03 12:09:38 +0500

    edit sidebar

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,7 +1,7 @@
 
 export const leftBarTabs = [
     {
-   heading : "Agency",
+   heading : "Agency Management",
    subheading: ["Add Agency", "View Agency","User Management"],
   },
   {
@@ -9,8 +9,40 @@ export const leftBarTabs = [
     subheading: ["Booking Managemnt", "Lead Management"],
    },
    {
-    heading : "Admin",
-    subheading: ["Booking Managemnt", "Lead Management"],
-   }
+    heading : "Administration",
+    subheading: ["Product List", "commerical plane" ,"Migration","supplier Management","Tax Configuration","Flight Rule Configuration","Command Builder","Location Setting","Security Management",],
+   },
+   {
+    heading : "Organization Management",
+    subheading:["rganization Staff",]
+   },
+   {
+    heading : "Role Management",
+    subheading:["Role Management","User Access Unmapping"]
+   },
+   {
+    heading : "Report",
+    subheading:["Transaction Report","Wallet statement","Customer Statement"]
+   },
+   {
+    heading : "Account",
+    subheading:["ROE","Payment Receipt","Wallet Adjustment"]
+   },
+   {
+    heading : "CMS",
+    subheading:["Content Pages"]
+   },
+   {
+    heading : "Language management",
+    subheading:["Languages List","Translation Manager"]
+   },
+   {
+    heading : "Theme",
+    subheading:["Theme Management"]
+   },
+   {
+    heading : "Notification",
+    subheading:["Email Trigger Settings"]
+   }, 
 ]
 
