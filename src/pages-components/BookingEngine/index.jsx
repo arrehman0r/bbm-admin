42284135b8a979c234f41db117fa24d521d21545
usmaniqbal8625 <137871476+usmaniqbal8625@users.noreply.github.com>
2024-09-05 19:32:04 +0500

    features/booking-engine

--- src/pages-components/BookingEngine/index.jsx
+++ src/pages-components/BookingEngine/index.jsx
@@ -8,6 +8,8 @@ import AppRadioButtons from '../../components/common/AppRadioButtons';
 import AppButton from '../../components/common/AppButton';
 import SearchIcon from '@mui/icons-material/Search';
 import PassengerCount from '../../components/PassengerCount';
+import { AspectRatio, Avatar, Card, Typography } from '@mui/joy';
+import { Opacity } from '@mui/icons-material';
 
 const BookingEngine = () => {
     const [tripOption, setTripOption] = useState('One Way');
@@ -18,12 +20,39 @@ const BookingEngine = () => {
 
     ];
 
+    const cards = [
+        {
+            title: 'Download Bookme App', src: "/static/images/avatar/1.jpg", description: 'Find the best deals on our mobile app', alt: 'a'
+        },
+        { title: 'Help Center', src: "/static/images/avatar/1.jpg", description: 'Contact with our support team', alt: 'a' },
+        { title: 'Manage Bookings', src: "/static/images/avatar/1.jpg", description: 'View and manage your bookings', alt: 'a' },
+    ]
+
+    const images = [
+        {
+            src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
+            title: 'Night view',
+            description: '4.21M views',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
+            title: 'Lake view',
+            description: '4.74M views',
+        },
+        {
+            src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
+            title: 'Mountain view',
+            description: '3.98M views',
+        },
+    ];
+
+
 
     const handleTripChange = (event) => {
         setTripOption(event.target.value);
     };
 
-    const handleOpenPassengerCount = ()=> {
+    const handleOpenPassengerCount = () => {
         setIsPopoverOpen(!isPopoverOpen)
     }
     return (
@@ -56,7 +85,7 @@ const BookingEngine = () => {
                         <AppDatePicker startDecorator={<FlightLandIcon />} placeholder="Arrival Date" />
                     </Box>}
                 <Box sx={{ flex: 1 }}>
-                    <PassengerCount isPopoverOpen = {isPopoverOpen} handleOpenPassengerCount={handleOpenPassengerCount} />
+                    <PassengerCount isPopoverOpen={isPopoverOpen} handleOpenPassengerCount={handleOpenPassengerCount} />
                 </Box>
                 <Box sx={{ flex: 1 }}>
                     <AppButton startDecorator={<SearchIcon />} text="Search Flight" variant="contained"
@@ -67,6 +96,69 @@ const BookingEngine = () => {
                 </Box>
 
             </Box>
+
+            <Box sx={{ marginTop: 10, display: 'flex', justifyContent: 'space-between', width: '95%' }}>
+                {cards.map((card, index) => (
+                    <Card sx={{ width: 400 }} key={index}>
+                        <div style={{ display: "flex", justifyContent: "space-around", gap: '10px' }}>
+                            <div>
+                                <Avatar src={card?.src} />
+                            </div>
+                            <div>
+                                <Typography variant="h6">{card.title}</Typography>
+                                <Typography variant="body2">{card.description}</Typography>
+
+                            </div>
+                        </div>
+                    </Card>
+                ))}
+
+
+
+            </Box>
+
+            <Typography level='h2' style={{ marginTop: '50px' }}>Latest Offers</Typography>
+            <Typography level='h5' sx={{ fontSize: 'xl' }}>We provide the best and most affordable services in Pakistan.</Typography>
+
+            <div style={{ width: "95%", display: 'flex', justifyContent: 'space-between', marginTop: "80px" }}>
+                {images.map((image) => (
+                    <AspectRatio ratio="2" sx={{ minWidth: 400 }}>
+                        <img
+                            srcSet={image.src}
+                            src={image.src}
+                            style={{ borderRadius: '10px' }}
+                        />
+                    </AspectRatio>
+                ))}
+
+
+            </div>
+
+
+            <Box style={{ backgroundColor: 'black', height: '350px', width: '100%', marginTop: '60px', padding: '25px' }}>
+                <div style={{ padding: '25px' }} >
+                    <Typography level='h2' style={{ color: "white" }}>Why Use Bookme?</Typography>
+                    <Typography level='h5' sx={{ fontSize: 'xl', color: "white" }}>We provide the best deals in Pakistan.</Typography>
+                </div>
+                <div style={{ display: "flex", marginTop: '20px' , justifyContent:"space-between" }}>
+                    {cards.map((card, index) => (
+                        <Card sx={{ width: 350, border: "none", backgroundColor: "transparent" }} key={index}>
+                            <div style={{ display: "flex", justifyContent: "space-around", gap: '10px' }}>
+                                <div>
+                                    <Avatar src={card?.src} />
+                                </div>
+                                <div>
+                                    <Typography variant="h6" level="h3">{card.title}</Typography>
+                                    <Typography variant="body2" level="h4">{card.description}</Typography>
+
+                                </div>
+                            </div>
+                        </Card>
+                    ))}
+
+                </div>
+            </Box>
+
         </Box>
     );
 };
